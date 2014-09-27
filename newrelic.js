0c1df480533b852c8749931fe7f560293c960937
shiki49 <shiki49.com@gmail.com>
2014-09-27 02:55:17 +0000

    first commit

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['isucon4'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'fe4b4df216565c1fc6ce8f02ae14f78b9c41a12d',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
